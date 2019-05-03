4ae3e9746a3101d17c8b75493c8c6be12f509abd
osdevisnot <osdevisnot@gmail.com>
2019-05-03 16:44:03 -0700

    router example

--- examples/router-with-lit/src/index.js
+++ examples/router-with-lit/src/index.js
@@ -1,11 +1,29 @@
 import { render, html } from 'https://unpkg.com/lit-html';
+import { match } from '../node_modules/@vanillajs/router/dist/router.mjs';
 
-const app = () => {
-  return html`
-    <div class="container">Hello world !!</div>
+const home = () =>
+  html`
+    <div>This is HOME component</div>
   `;
-};
+const about = () =>
+  html`
+    <div>This is ABOUT component</div>
+  `;
+const profile = params =>
+  html`
+    <div>This is PROFILE component ${params.id}</div>
+  `;
 
-const flush = () => render(app(), document.body);
+const routes = [
+  { path: '/', render: home },
+  { path: '/about', render: about },
+  { path: '/profile/:id', render: profile }
+];
+
+const flush = () => {
+  let { route, params, query } = match(routes, window.location);
+  console.log('route, params, query : ', route, params, query);
+  render(route.render(params, query), document.body);
+};
 
 flush();
