1e3bcf05a780772fb1f161ab23b532552ae6590a
Abhishek Shende <osdevisnot@gmail.com>
2019-11-14 17:21:30 -0800

    add example for store

--- packages/store/src/store.js
+++ packages/store/src/store.js
@@ -10,12 +10,12 @@ let update = (action, state) => {
 let dispatch = (action, ...payload) => {
 	return event => {
 		if (_actions[action]) {
-			let update = _actions[action](event, _state, ...payload)
-			if (update) {
-				if (update.then) {
-					return update.then(res => update(res, action))
+			let result = _actions[action](event, _state, ...payload)
+			if (result) {
+				if (result.then) {
+					return result.then(response => update(action, response))
 				}
-				return update(update, action)
+				return update(action, result)
 			}
 		}
 	}
@@ -32,7 +32,7 @@ let store = {
 	get state() {
 		return _state
 	},
-	use(state, actions) {
+	init(state, actions) {
 		_state = { ..._state, ...state }
 		_actions = { ..._actions, ...actions }
 	},
